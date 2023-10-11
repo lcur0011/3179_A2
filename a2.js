d6620b723130967f37965e1606670a05af2c1e6b
lcur0011 <147512231+lcur0011@users.noreply.github.com>
2023-10-11 16:09:51 +1100

    Add files via upload

---
+++ a2.js
@@ -0,0 +1,6 @@
+var idiom_1 = "bar.json";
+vegaEmbed("#bar", idiom_1).then(function(result){}).catch(console.console.error);
+
+var idiom_2 = "map.json";
+vegaEmbed("#mappy", idiom_2).then(function(result){}).catch(console.console.error);
+
